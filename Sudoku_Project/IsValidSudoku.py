46a4c03b06c500365f6845d13da07c1c0805666c
Gabriel Urena <54905586+GabeUrena@users.noreply.github.com>
2023-12-14 10:50:43 -0500

    Debugging

--- Sudoku_Project/IsValidSudoku.py
+++ Sudoku_Project/IsValidSudoku.py
@@ -1,16 +1,41 @@
 import numpy as np
 class IsValidSudoku:
-    __sudokuPuzzle = [[0]*9]*9
-    
+    __sudokuPuzzle = [ [0, 0, 0, 0, 0, 0, 0, 0, 0],
+			    [0, 0,0 , 0, 0, 0, 0, 0, 0],
+				[0, 0, 0, 0, 0, 0, 0, 0, 0],
+				[0, 0, 0, 0, 0, 0, 0, 0, 0],
+				[0, 0, 0, 0, 0, 0, 0, 0, 0],
+				[0, 0, 0, 0, 0, 1, 0, 0, 0],
+				[0, 0, 0, 0, 0, 6, 0, 0, 0],
+				[0, 0, 0, 0, 0, 0, 0, 0, 0],
+				[0, 0, 0, 0, 0, 0, 0, 0, 0] ]
+    print(__sudokuPuzzle)
+        
+        
+        
+        
+        
+        
         
     def __init__(self, puzzle):
-    #Copy 2D array to the private array   
-        for x in range(1, 9):
-            for y in range(1, 9):
-    #Check if out of bounds
-                if(puzzle[x][y] <= 0 or puzzle[x][y] > 9):
-                    print(f'Index [{x}][{y}] is out of range.\nMust be between 1 and 9\n')
-                self.__sudokuPuzzle[x][y] = puzzle[x][y]
+    
+        for row in range(0, 9):
+            for col in range(0, 9):
+                print(f'puzzle at index [{row},{col}] is {puzzle[row][col]}')
+                print (f'sudokuPuzzle at [{row},{col}] is {self.__sudokuPuzzle[row][col]}')
+                self.__sudokuPuzzle[row][col] = puzzle[row][col]
+                print(self.__sudokuPuzzle)            
+        print()
+                    
+        
+        
+        
+        
+        
+        
+        
+                    
+                    
                     
                     
     #3x3 grid check
@@ -35,21 +60,9 @@ class IsValidSudoku:
     def rowCheck(self):
         print("Running row check")
     # check entire board
-        for row in range(1,9):
-            for col in range(1,9):
-    #grab number and check if it repeats
-                temp = self.__sudokuPuzzle[row][col]
-                print(f'{temp} is the temp')
-                occur = 0
-                print(f'{occur} occurances')
-                print('start of for loop')
-                for x in range(1,9):
-                    if temp == self.__sudokuPuzzle[row][x]:
-                        print(f'{temp} is equal to sudokuPuzzle: {self.__sudokuPuzzle[row][x]}')
-                        occur += 1
-                        print(occur)
-                        if occur > 1:
-                            return False
+        for row in range(0,9):
+            for col in range(0,9):
+                print(self.__sudokuPuzzle[row][col])
         return True
                     
                     
@@ -80,11 +93,13 @@ puzzleInput = [ [1, 1, 1, 1, 1, 1, 1, 1, 1],
 				[3, 7, 1, 1, 5, 6, 8, 4, 2],
 				[4, 9, 6, 1, 8, 2, 5, 7, 3],
 				[2, 8, 5, 4, 7, 3, 9, 1, 6] ]
-        
+
+
+                
 sudokuValidator = IsValidSudoku(puzzleInput)
         
 #print(sudokuValidator.columnCheck())
-print(sudokuValidator.rowCheck())
+#print(sudokuValidator.rowCheck())
 #print(sudokuValidator.gridCheck())
         
         
